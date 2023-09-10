a9cc94f0973b0e35b3cd3ebe1753d9600abbb16d
elizabeth-akinbiyi <141285860+elizabeth-akinbiyi@users.noreply.github.com>
2023-09-10 23:59:56 +0100

    Add files via upload

---
+++ Script/script.js
@@ -0,0 +1,36 @@
+//Function to update the current day of the week in Nigeria (WAT time zone)
+function updateCurrentDayOfTheWeek() {
+    const daysOfWeek = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+    ];
+    const currentDate = new Date();
+    const options = {weekday: 'long', timeZone: 'Africa/Lagos'};// Nigeria's time zone
+    const currentDay = new Intl.DateTimeFormat('en-US', options).format(currentDate);
+    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = currentDay;}
+    
+    //Initial update
+    updateCurrentDayOfTheWeek();
+
+    //Update the current day of the week every 24 hours
+    setInterval(updateCurrentDayOfTheWeek, 86400000);
+
+    //Function to update the current local time in Nigeria (WAT time zone)
+    function updateCurrentLocalTime(){
+        const currentDate = new Date();
+        const options = {timeZone: 'Africa/Lagos'}; // Nigeria's time zone
+        const localTime = currentDate.toLocaleTimeString('en-US', options);
+        const utcTimeMilliseconds = currentDate.getTime(); // Get UTC time in milliseconds
+        document.querySelector('[data-testid="currentUTCTime"]').textContent =
+        'Local Time: ' + localTime + ' | UTC Time(ms): ' + utcTimeMilliseconds;}
+
+    // Initial update
+    updateCurrentLocalTime();
+
+    // Update the current local time every second
+    setInterval(updateCurrentLocalTime, 1000);
